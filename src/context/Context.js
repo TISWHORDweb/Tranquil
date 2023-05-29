46521abf4fcbf52219146d717e4dd7e5916d88cd
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-05-29 02:22:50 +0100

    few changes

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,14 +1,11 @@
 // MyContext.js
 import React, { createContext, useState } from 'react';
 
-// Create the context
 export const MyContext = createContext();
 
-// Create a provider component
 export const MyContextProvider = ({ children }) => {
   const [data, setData] = useState('');
 
-  // Function to update the data value
   const updateData = (newValue) => {
     setData(newValue);
   };
